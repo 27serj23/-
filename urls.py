f565454d4c22585fcf8f2b44adebbe420a00ce25
27serj23 <154461692+27serj23@users.noreply.github.com>
2024-05-13 14:09:02 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,11 +1,23 @@
-from django.urls import path
-from . import views
+"""
+URL configuration for newKino project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.blog, name='list'),
-    path('post/<int:pk>/', views.detail, name='detail'),
-    path('post/create/', views.create, name='create'),
-    path('post/<int:pk>/manage/', views.manage, name='manage'),
-    path('post/<int:pk>/comment/add/', views.add_comment, name='add_comment'),
-    path('comment/<int:pk>/edit/', views.edit_comment, name='edit_comment'),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
